472d70afb5d3e143382c6ee44e0f5d47443a7940
Dominik <dominik.wolters@sinsheim-ost.de>
2025-10-17 10:25:40 +0200

    Started project, added start page to add name and link

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function AddAlbum(){
+  function handleForward(a){
+    a.preventDefault();
+
+    const form = a.target;
+    const formData = new FormData(form);
+
+    const album = Object.fromEntries(formData.entries());
+    AddMoods(album);
+  }
+  return (   
+    <div className='addAlbum'>
+      <form method="post" onSubmit={handleForward}>
+        <label htmlFor='link-input'>
+          <input id='input' name='name' placeholder='Name'/>
+          <input id='input' name='Link' placeholder='AlbumLink'/>
+          <button type='submit'>Continue</button>
+        </label>
+      </form>
+    </div>
+  )
+} 
+
+function AddMoods(album){
+  const name = album.name;
+  return (
+    <div className='addMoods'>
+      <p>{album.name}</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+      <AddAlbum/>
+    </>
+  )
+}
+
+export default App
