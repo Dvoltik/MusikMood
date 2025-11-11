48fde1ba4d4f1329657b467c120831ce4e8dd098
Dominik <dominik.wolters@sinsheim-ost.de>
2025-11-11 12:14:18 +0100

    fehler beim einfügen und entfernen von genres. Entfernen und neu rendern funktioniert nicht richtig

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,58 @@ function AddAlbum(){
     const album = Object.fromEntries(formData.entries());
     //AddMoods(album);
   }
-  function addGenre(){
+  const [genres, setGenres] = useState([]);
+  
+  function addGenre(e){
+    e.preventDefault();
     const table = document.getElementById('genre-wrapper');
 
     var row = table.insertRow(0);
     const genre = prompt('Welches Genre möchtest du hinzufügen?');
+    
     if (genre && genre.trim() !== '') {
-      setGenres([...genres, genre.trim()]);
+      genres.push(genre);
+      updateGenre();
     }
-    row.innerHTML = "test";
+  }
+
+  function updateGenre(){
+    document.getElementById('genre-table').innerHTML = ' ';
+    const tbody = document.getElementById('genre-table');
+    const row = document.createElement('tr')
+    const displayGenre = document.createElement('td');
+    const deleteGenre = document.createElement('td');
+    const bttn = document.createElement('button');
+    let id = 0;
+
+    genres.forEach(()=>{
+      row.id = id;
+      displayGenre.textContent = genres[id];
+      bttn.textContent = '-';
+      bttn.type = 'button';
+      bttn.classList.add("order-" + id);
+      bttn.addEventListener('click', () => {
+        
+        removeGenre(id);
+      });
+      console.log(id);
+      deleteGenre.appendChild(bttn);
+      row.appendChild(displayGenre);
+      row.appendChild(deleteGenre);
+      tbody.appendChild(row);
+      id++;
+    });
+    //console.log(genres);
+  }
+
+  const removeGenre = (index) => {
+    console.log("removeCalled");
+    console.log(index);
+    var test = genres.splice(index, 1);
+    console.log(test);
+    updateGenre();
+    
+    
   }
   return (   
     <div className='addAlbum'>
@@ -42,7 +85,9 @@ function AddAlbum(){
             <fieldset className='mid-row-wrapper'>
               <legend>Genres</legend>
               <table id='genre-wrapper' className='table-wrapper'>
-                
+                <tbody id='genre-table'>
+                    
+                </tbody>
               </table>
               
               <button onClick={addGenre} className='buttons'>+ Genre</button>
